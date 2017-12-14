ae2485022bcb99842790df7c410ec825fd066b7d
Leonardo Jose  Schettini de Arruda <a1601323@unet.univie.ac.at>
2017-12-13 22:36:59 -0300

    Implement basic main script to perform sentiment analysis

--- src/main.py
+++ src/main.py
@@ -4,27 +4,43 @@ import praw
 
 from datetime import datetime, date
 
-from models.coin_chart import CoinChart, Candle
+from preprocessment.cleaner_builder import cleaner_builder
+
+from models.coin_chart import CoinChart
 from models.subreddits import Subreddit
 
-start_date = datetime(2016, 11, 8, 8)
-end_date = datetime(2016, 11, 9, 8)
+from features.sentiment.analyser import SentimentAnalyser
+
+start_date = datetime(2016, 11, 8, 7)
+end_date = datetime(2016, 11, 8, 23)
 
 print ('Building coin chart\n')
 
 coin_chart = CoinChart(start_date=start_date, 
 	end_date=end_date, 
-	time_frame='12h', 
+	timeframe='12h', 
 	chart_exchange='tBTCUSD')
 
 print ('Starting subreddit comments retrieval\n')
 
+comment_cleaner = lambda document: cleaner_builder(
+	document, 
+	lower_words = False,
+	fold_numbers = False,
+	remove_stopwords = False, 
+	do_stemming = False, 
+	do_lemmatizing = False)
+
 subreddits = Subreddit(client_id='g7g1TXRPCG1t4g', 
 	client_secret='wMUyCgmjorwGoY9aOCE5jJm6Q3w', 
 	user_agent='testscript by /u/mariocbsf', 
-	title='Bitcoin', 
 	start_date=start_date, 
 	end_date=end_date, 
-	interval=8640) # 4320 is Equivalent to 12 hours in seconds
+	title='Bitcoin', 
+	comment_cleaner=comment_cleaner,
+	interval=43200
+) # 43200 is Equivalent to 12 hours in seconds 
 
 print ('It\'s finished')
+
+sa = SentimentAnalyser()
