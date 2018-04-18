33cabd7c3a308bb4b98c1fb034448936e5955767
Leonardo Jose  Schettini de Arruda <leoschettini2@gmail.com>
2018-04-18 11:59:52 -0300

    Update cleaner builder

--- src/preprocessment/cleaner_builder.py
+++ src/preprocessment/cleaner_builder.py
@@ -18,27 +18,26 @@ english_stopwords = nltk.corpus.stopwords.words('english')
 porter_stemmer = nltk.stem.PorterStemmer()
 wordnet_lemmatizer = WordNetLemmatizer()
 
-def cleaner_builder(text, lower_words=False, fold_numbers=False, remove_stopwords=False, do_stemming=False, do_lemmatizing=False):
-	tagged_words = _extract_tagged_words(text)
-	
-	if (lower_words):
-		tagged_words = [word.lower() for (word, postag) in tagged_words]
+def cleaner_builder(document, lower_words=True, fold_numbers=False, remove_stopwords=False, do_stemming=True, do_lemmatizing=False):
+    
+    tagged_words = _extract_tagged_words(document)
+    if (lower_words):
+        tagged_words = [(word.lower(),postag) for (word, postag) in tagged_words]
 
-	if (fold_numbers):
-		tagged_words = ['NUM' if re.match(BEGIN_NUMBER_REGEX, word) else word for (word, postag) in tagged_words]
-	
-	if (remove_stopwords):
-		tagged_words = [word for (word, postag) in tagged_words if word not in english_stopwords]
-	
-	if (do_stemming):
-		tagged_words = [porter_stemmer.stem(word) for (word, postag) in tagged_words]
+    if (fold_numbers):
+        tagged_words = ['NUM' if re.match(BEGIN_NUMBER_REGEX, word) else word for (word, postag) in tagged_words]
+    
+    if (remove_stopwords):
+        tagged_words = [(word,postag) for (word, postag) in tagged_words if word not in english_stopwords]
+    
+    if (do_stemming):
+        tagged_words = [(porter_stemmer.stem(word), postag) for (word, postag) in tagged_words]
 
-	if (do_lemmatizing):
-		tagged_words = [wordnet_lemmatizer.lemmatize(word, pos='n') for (word, postag) in tagged_words]
+    if (do_lemmatizing):
+        tagged_words = [(wordnet_lemmatizer.lemmatize(word, pos='n'), postag) for (word, postag) in tagged_words]
 
-	min_length = 3
-
-	return [(term, postag) for (term, postag) in tagged_words if BEGIN_CHAR_REGEX.match(term) and len(term) >= min_length]
+    min_length = 3
+    return [(term, postag) for (term, postag) in tagged_words if BEGIN_CHAR_REGEX.match(term) and len(term) >= min_length]
 
 def _extract_tagged_words(text):
 	values = [] # find use for values
